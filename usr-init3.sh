07336a1827736f6ff9b9ef9e8dd97d21a58f0dce
OneAutumnSheath <info@oneautumnsheath.de>
2023-04-19 20:35:33 +0200

    Update usr-init3.sh

--- usr-init3.sh
+++ usr-init3.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-wget https://cdn.oneautumnsheath.de/bashrc -O /root/.bashrc
-chmod -x /etc/update-motd.d/*
-apt-get update
-apt-get full-upgrade -y
-apt-get install neofetch -y
-rm /etc/motd
-touch /etc/motd
-touch /root/.hushlogin
-bash -c $'if [ ! -f "/etc/profile.d/mymotd.sh" ]; then echo "neofetch" > /etc/profile.d/mymotd.sh; fi && chmod +x /etc/profile.d/mymotd.sh'
 
+if [ ! -f "/etc/.completed_setup" ]; then
+    wget https://cdn.oneautumnsheath.de/bashrc -O /root/.bashrc
+    chmod -x /etc/update-motd.d/*
+    apt-get update
+    apt-get full-upgrade -y
+    apt-get install neofetch -y
+    rm /etc/motd
+    touch /etc/motd
+    touch /root/.hushlogin
+    bash -c $'if [ ! -f "/etc/profile.d/mymotd.sh" ]; then echo "neofetch" > /etc/profile.d/mymotd.sh; fi && chmod +x /etc/profile.d/mymotd.sh'
+    touch /etc/.completed_setup
+fi
